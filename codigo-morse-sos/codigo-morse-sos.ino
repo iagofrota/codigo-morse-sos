b98126691b3016f24b9a3729a589ba173e7011df
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-01 23:30:02 -0300

    foi implementado o código morse usando um led

--- codigo-morse-sos/codigo-morse-sos.ino
+++ codigo-morse-sos/codigo-morse-sos.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LED_VERMELHO 2
 
+void setup() {
+  iniciarPortas();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  piscarLedRapido();
+  piscarLedDevagar();
 }
+
+void iniciarPortas() {
+  pinMode(LED_VERMELHO, OUTPUT);
+}
+
+// Três piscadas rápidas
+void piscarLedRapido() {
+  for (int i = 0; i < 3; i++) {
+    piscarLedLigado();
+    delay(150);
+    piscarLedDesligado();
+    delay(100);
+  }
+}
+
+void piscarLedDevagar() {
+  for (int i = 0; i < 3; i++) {
+    piscarLedLigado();
+    delay(400);
+    piscarLedDesligado();
+    delay(100);
+  }
+}
+
+void piscarLedLigado() {
+  digitalWrite(LED_VERMELHO, HIGH);
+}
+
+void piscarLedDesligado() {
+  digitalWrite(LED_VERMELHO, LOW);
+}
+
